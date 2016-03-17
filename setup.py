0f065e748cf0fd869a513c9b4d11ae2ef1af14f8
vsoch <vsochat@stanford.edu>
2016-03-17 15:48:07 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Application name:
+    name="repofish",
+
+    # Version number (initial):
+    version="0.0.1",
+
+    # Application author details:
+    author="Vanessa Sochat",
+    author_email="vsochat@stanford.edu",
+
+    # Packages
+    packages=find_packages(),
+
+    # Data
+    package_data = {'repofish.lib':['*.json']},
+
+    # Details
+    url="http://www.github.com/vsoch/repofish",
+
+    license="LICENSE",
+    description="search github repos for python functions and generate standard data structures for them",
+
+    install_requires = ['numpy','pandas']
+)
