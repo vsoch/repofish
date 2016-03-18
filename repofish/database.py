224031bb8f21d2b35908d8e5eed0e6b9c0ebea54
vsoch <vsochat@stanford.edu>
2016-03-18 16:47:29 -0700

    updating function to fix parsing errors

--- repofish/database.py
+++ repofish/database.py
@@ -6,7 +6,6 @@ def get_functions(module_folder):
     '''get_functions: parses a python module folder, and returns a data structure with arguments and defaults
     '''
     module_folder = os.path.abspath(module_folder)
-
     functions = dict()
     for root, subs, files in os.walk(module_folder):
         basename = os.path.basename(root)
@@ -16,28 +15,60 @@ def get_functions(module_folder):
             py_files = [x for x in py_files if not re.search("^_|^[.]",x)]
             # Add functions to data structure
             for py_file in py_files:
+                module_functions = dict()
+                classes = dict()
                 module = os.path.splitext(py_file)[0]
                 filey = open("%s/%s" %(root,py_file),"rb")
                 lines = filey.read().splitlines()
-                lines = [x.replace("def","").strip() for x in lines if re.search("def",x)]  
-                for function in lines:
-                    if not re.search("^_",function):
-                        try: # lines with "def" that aren't functions
-                            function_name,function_args = function.split("(")
-                            print "Adding function %s" %(function_name)
-                            function_args = [x.strip() for x in function_args.strip("):").split(",") if x != "self"]
-                            # Get defaults
-                            arg_list = []
-                            for function_arg in function_args:       
-                                if re.search("=",function_arg):
-                                    arg_value,arg_default = function_arg.split("=")
-                                    arg_list.append({"name":arg_value,
-                                                    "default":arg_default})
-                                else:                              
-                                    arg_list.append({"name":function_arg})
-                            functions[function_name] = dict()
-                            functions[function_name]["args"] = arg_list 
-                        except:
-                            pass    
+                # First split code up by (likely) classes
+                class_idx = [x for x in range(len(lines)) if re.search("^class ",lines[x].strip())]
+                if len(class_idx) == 0:
+                    class_idx = [len(lines)-1]  
+                class_idx.insert(0,0)
+                for c in range(len(class_idx)-1):
+                    start_idx = class_idx[c]
+                    end_idx = class_idx[c+1]     
+                    new_functions = get_functions_lines(lines[start_idx:end_idx])
+                    # If it's a class, append to classes
+                    if re.search("^class ",lines[start_idx].strip()):
+                        class_name = get_class_name(lines[start_idx])
+                        classes[class_name] = new_functions
+                    else:
+                        module_functions.update(new_functions)
+                # Add functions based on module
+                classes["*"] = module_functions
+                functions[module] = classes
+    return functions
 
+def get_class_name(classline):
+    classline = classline.replace("class","").strip()
+    return classline.split("(")[0]
+
+def get_functions_lines(lines):
+    functions = dict()
+    while len(lines) > 0:
+       line = lines.pop(0).strip()
+       # Functions outside of class
+       if re.search("^def ",line):
+           if not re.search("^#",line):
+               function_name,function_args = line.split("(",1)
+               function_name = function_name.split(" ")[-1]
+               print "Adding function %s" %(function_name)
+               # If no closing parens is present, we have to continue until we find one
+               while not re.search("[)]:",function_args):
+                   nextline = lines.pop(0)
+                   function_args = "%s, %s" %(function_args,nextline)
+               function_args = function_args.strip(":")
+               function_args = [x.strip() for x in function_args.strip("):").split(",") if x != "self"]
+               # Get defaults
+               arg_list = []
+               for function_arg in function_args:       
+                   if re.search("=",function_arg):
+                       arg_value,arg_default = function_arg.split("=",1)
+                       arg_list.append({"name":arg_value,
+                                       "default":arg_default})
+                   else:                              
+                       arg_list.append({"name":function_arg})
+               functions[function_name] = dict()
+               functions[function_name]["args"] = arg_list                        
     return functions
--- repofish/utils.py
+++ repofish/utils.py
@@ -1,9 +1,53 @@
+from Bio import Entrez
+from cognitiveatlas.api import get_concept
+import pickle
 import json
+import time
 import os
+import sys
 
 def save_json(json_obj,output_file):
     filey = open(output_file,'wb')
     filey.write(json.dumps(json_obj, sort_keys=True,indent=4, separators=(',', ': ')))
     filey.close()
     return output_file
+
+
+def search_pubmed(term,retstart=0,retmax=100000):
+    '''search_pubmed returns a record for a search term using Entrez.esearch
+    :param term: the term to search for
+    :param retstart: where to start retrieving results, default is 0
+    :param retmax: the max number of results to return, default is 100K
+    '''
+    handle = Entrez.esearch(db='pubmed',term=str(term),retstart=retstart,retmax=retmax)
+    record = Entrez.read(handle)
+    handle.close()
+    return record
+
+def search_articles(email,term,retmax=100000):
+    '''search_articles returns a list of articles associated with a term
+    :param email: an email address to associated with the query for Entrez
+    :param term: the search term for pubmed, "+" will replace spaces
+    :param retmax: maximum number of results to return, default 100K. If more exist, will be obtained.
+    '''
+    Entrez.email = email
+    # Replace spaces in term with +
+    term = term.replace(" ","+")
+    record = search_pubmed(term)
+    if "IdList" in record:
+        number_matches = int(record["Count"])
+        allrecords = record["IdList"]
+        start = 100000
+        # Tell the user this term is going to take longer
+        if number_matches >= 100000:
+            print "Term %s has %s associated pmids" %(term,number_matches)
+        while start <= number_matches:
+            record = search_pubmed(term,retstart=start)
+            allrecords = allrecords + record["IdList"]
+            start = start + 100000
+            time.sleep(0.5)
+        return allrecords
+    else:
+        return []
+
 
