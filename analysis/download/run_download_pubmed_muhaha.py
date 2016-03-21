d64cec6649aa29f0fa2bb371ed57f05b31303fc7
vsoch <vsochat@stanford.edu>
2016-03-21 16:18:35 -0700

    adding code to run on TACC

--- analysis/download/run_download_pubmed_muhaha.py
+++ analysis/download/run_download_pubmed_muhaha.py
@@ -7,6 +7,7 @@ import pickle
 import os
 
 base = "/scratch/PI/russpold/data/PUBMED"
+base = "/work/02092/vsochat/pubmed"
 
 # Save the pickled ftp and pmcids
 ftp_pickle = "%s/ftp_df.pkl" %(base)
@@ -23,11 +24,12 @@ batch_size = 500
 iters = len(pc_ids)/batch_size
 
 # We will download them here
-download_folder = "%s/article" %(base)
+download_folder = "%s/articles" %(base)
 
 if not os.path.exists(download_folder):
     os.mkdir(download_folder)
 
+# SHERLOCK
 # Prepare and submit a job for each
 for i in range(iters):
     download_subfolder = "%s/%s" %(download_folder,i)
@@ -49,3 +51,23 @@ for i in range(iters):
     filey.writelines("python /home/vsochat/SCRIPT/python/repofish/analysis/download/download_pubmed_muhaha.py %s %s %s %s %s\n" % (start,end,download_subfolder,ftp_pickle,ids_pickle))
     filey.close()
     os.system("sbatch -p russpold --qos russpold .job/%s.job" % (jobname))
+
+
+# TACC
+homebase = "%s/SCRIPT/python/repofish/analysis/download" %os.environ["HOME"]
+output_file = "%s/pmclaunch.job" %homebase
+filey = open(output_file,"w")
+
+# Prepare and submit a job for each
+for i in range(iters):
+    download_subfolder = "%s/%s" %(download_folder,i)
+    if not os.path.exists(download_subfolder):
+        os.mkdir(download_subfolder)
+    start = i*batch_size
+    if i != iters:
+        end = start + batch_size
+    else:
+        end = len(pc_ids)
+    filey.writelines("python %s/download_pubmed_muhaha.py %s %s %s %s %s\n" % (homebase,start,end,download_subfolder,ftp_pickle,ids_pickle))
+
+filey.close()
