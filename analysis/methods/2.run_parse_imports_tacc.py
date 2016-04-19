05a914732bc31bb4bc13986f60956de3280126e9
vsoch <vsochat@stanford.edu>
2016-04-19 12:36:56 -0700

    starting to parse inputs

--- analysis/methods/2.run_parse_imports_tacc.py
+++ analysis/methods/2.run_parse_imports_tacc.py
@@ -1,9 +1,11 @@
 from glob import glob
 import json
 import pandas
+import requests
 import numpy
 import pickle
 import os
+import re
 
 home = os.environ["HOME"]
 base = "%s/data/pubmed" %os.environ["LAB"]
@@ -16,8 +18,6 @@ if not os.path.exists(outfolder):
 
 files = glob("%s/*.json" %repo_folder)
 
-jobfile = "%s/parsereopos.job" %(scripts)
-filey = open(jobfile,'w')
 
 # Take a look at repo urls to help parsing
 urls = []
@@ -40,6 +40,99 @@ inputs = dict()
 inputs["urls"] = urls
 inputs["pmids"] = pmids
 pickle.dump(inputs,open("%s/inputs.pkl" %outfolder,"wb"))
+
+# inputs = pickle.load(open("%s/inputs.pkl" %outfolder,"rb"))
+
+# Zip together
+inputs = zip(inputs["pmids"],inputs["urls"])
+
+# Remove links that are just to github
+inputs = [x for x in inputs if not re.search("[http|https]://github.com$",x[1])]
+inputs = [x for x in inputs if not re.search("[http|https]://www.github.com$",x[1])]
+
+# Find urls that don't match pattern github.com/user/repo
+needs_curation = [x for x in inputs if not re.search("[http|https]://(www.)?github.com/.*/.*$",x[1])]
+inputs = [x for x in inputs if re.search("[http|https]://(www.)?github.com/.*/.*$",x[1])]
+
+# We will need to parse links based on type
+raw_files = []
+gists = []
+github_io = []
+github_io_master = []
+rest = []
+nbviewer = []
+users = []
+manual_curation = []
+github_help = []
+
+while len(needs_curation) > 0:
+    element = needs_curation.pop()
+    # We've found a gist
+    if re.search("[http|https]://gist.github.com/.*$",element[1]):
+    #    print "GIST: %s" %element[1]
+        gists.append(element)
+    # Github help
+    elif re.search("[http|https]://help.github.com/.*$",element[1]):
+        github_help.append(element)
+    # Github user main pages
+    elif re.search("[http|https]://github.com/.*$",element[1]):
+        users.append(element)
+    # nbviewer
+    elif re.search("nbviewer",element[1]):
+        nbviewer.append(element)
+    # We've found a raw file
+    elif re.search("[http|https]://raw.github.com/.*$",element[1]):
+    #    print "RAW: %s" %element[1]
+        raw_files.append(element)
+    # github io address associated with repo
+    elif re.search("[http|https]://.*[.]github.io/.*$",element[1]):
+        github_io.append(element)
+    elif re.search("[http|https]://.*[.]github.com/.*$",element[1]):
+        github_io.append(element)
+    # github io address for entire association
+    elif re.search("[http|https]://.*[.]github.io$",element[1]):
+        github_io_master.append(element)
+    elif re.search("[http|https]://.*[.]github.com$",element[1]):
+        github_io_master.append(element)
+    #    print "IO: %s" %element[1]
+    else:
+        rest.append(element)
+
+# These are ready for parsing
+urls = dict()
+urls["raw_files"] = raw_files
+urls["gists"] = gists # will parse later
+urls["repos"] = inputs
+urls["github_io"] = github_io_master # we can't obtain specific repos
+urls["github_help"] = github_help
+urls["github_users"] = users
+urls["nbviewer"] = nbviewer # will parse later
+pickle.dump(urls,open("%s/inputs_categorized.pkl" %outfolder,"wb"))
+
+# For github_io urls, we need to convert to repo url
+while len(github_io) > 0:
+    element = github_io.pop()
+    url = element[1].replace("http://","").replace("https://","")
+    user_name =  url.split("/")[0].split(".")[0]
+    repo_name = url.split("/")[1]
+    new_url = "http://www.github.com/%s/%s" %(user_name,repo_name)
+    response = requests.get(new_url)
+    if response.status_code == 200:
+        element = (element[0],new_url)
+        urls["repos"].append(element)
+    else:
+        rest.append(element)
+
+# Manual curation
+
+
+pickle.dump(urls,open("%s/inputs_categorized.pkl" %outfolder,"wb"))
+
+
+
+jobfile = "%s/parsereopos.job" %(scripts)
+filey = open(jobfile,'w')
+
 
 seen = []
 for f in files:
