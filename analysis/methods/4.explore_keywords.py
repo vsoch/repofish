47bf4345275e432e2fdb6dd173e18778ecd933f9
vsoch <vsochat@stanford.edu>
2016-04-22 13:22:52 -0700

    adding keyword counts

--- analysis/methods/4.explore_keywords.py
+++ analysis/methods/4.explore_keywords.py
@@ -31,22 +31,52 @@ for f in files:
     
 keywords = numpy.unique(keywords).tolist()
 
+# Function to parse keywords
+def parse_keywords(kw):
+    if isinstance(kw,dict):
+        if "#text" in kw:
+            return kw["#text"]
+        elif "italic" in kw:
+            if "named-content" in kw["italic"]:
+                return kw["italic"]["named-content"]["named-content"]["#text"]
+            elif "#text" in kw["italic"]:
+                return kw["italic"]["#text"]
+            else:
+                return kw["italic"]
+        elif "named-content" in kw:
+            if isinstance(kw["named-content"]["named-content"],dict):
+                return kw["named-content"]["named-content"]["#text"]
+            return kw["named-content"]["named-content"][0]["#text"]
+        elif "styled-content" in kw:
+            return kw["styled-content"]["#text"]
+    return kw 
+
 # Some keywords are in italic
 keywords_updated = []
 for kw in keywords:
-    if isinstance(kw,dict):
-        if "italic" in kw:
-            if "named-content" in kw["italic"]:
-                word = keywords[0]["italic"]["named-content"]["named-content"]["#text"]
-                keywords_updated.append(word)
-    else:
-        keywords_updated.append(kw)
+    keywords_updated.append(parse_keywords(kw))
 
+# Also do a count
+df = pandas.DataFrame(columns=["count"])
+for f in files:
+    print "Adding %s to list" %(f)
+    result = json.load(open(f,'r'))
+    if "keywords" in result:
+        if not isinstance(result["keywords"],list):
+            kw_list = [result["keywords"]]
+        else:
+            kw_list = result["keywords"]
+        for kw in kw_list:
+            kw_parsed = parse_keywords(kw)
+            if kw_parsed in df.index:
+                df.loc[kw_parsed,"count"] = df.loc[kw_parsed,"count"] + 1
+            else:
+                df.loc[kw_parsed,"count"] = 1
 
-df=pandas.DataFrame()
-df["keywords"] = keywords_updated
-df.to_csv("%s/keywords_unique.tsv" %base,sep="\t",encoding="utf-8")
-df.to_json("%s/keywords_unique.json" %base)
+# Sort by counts
+df.to_csv("%s/keywords_counts.tsv" %base,sep="\t",encoding="utf-8")
+df.to_json("%s/keywords_counts.json" %base)
+
 
 # JOURNALS
 journals = []
